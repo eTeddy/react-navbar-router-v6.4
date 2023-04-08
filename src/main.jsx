d52b325fb0a8741a08c2bec95c4e1dac3650360d
eTeddy <tpetw886@gmail.com>
2023-04-07 20:23:26 -0500

    createHashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,83 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createHashRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import ErrorPage from "./routes/ErrorPage";
+
+const AppLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createHashRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
+
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
+import App from './App';
+import './styles/index.css';
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+**********************************************
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -8,3 +88,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+*/
